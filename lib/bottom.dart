1947510ffeaf4783026cfa9601eb890454f7485e
samehmaher <Samehnor12>
2021-06-11 02:34:45 +0200

    0.0.1

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 class ReusableBottom extends StatelessWidget {
-  ReusableBottom({required this.color});
+  ReusableBottom({required this.color,required this.buttonName});
   final Color color;
+  // final Function onPress;
+  final String buttonName;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: size.height*0.08 ,
-      width: size.width*0.08,
+      height: size.height*0.06 ,
+      width: size.width*0.7,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         color: color,
       ),
       child: TextButton(
-        onPressed: (){},
-        child: null,
-
+        onPressed: (){
+        },
+        child: Text(
+          buttonName,
+          style: TextStyle(fontSize: 22, color: Colors.white, height: 1.5).copyWith(fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,8 +1,7 @@
+import 'package:amber/bottom.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
-
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -11,6 +10,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -18,21 +18,85 @@ class _LoginScreenState extends State<LoginScreen> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Center(
-          child: Column(
-            children: [
-              Spacer(),
-              Container(
-                height: 340,
-                width: 340,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Colors.white.withOpacity(0.6),
-                ),
+        child: Column(
+          children: [
+            SizedBox(height: 200,),
+            Container(
+              padding: EdgeInsets.only(left: 40,right: 40,top: 40),
+              height: 340,
+              width: 340,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: Colors.white.withOpacity(0.5),
               ),
-              
-            ],
-          ),
+              child: Column(
+                children: [
+                  Form(child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Username :',
+                          labelStyle: TextStyle(
+                            color: Colors.red,
+                            fontSize: 20
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.red
+                              )
+                          ),
+
+
+                        ),
+
+                      ),
+                      SizedBox(height: 30,),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Password :',
+                            labelStyle: TextStyle(
+                                color: Colors.red,
+                                fontSize: 20
+                            ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.red
+                              )
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
+                        ),
+
+                      ),
+                      SizedBox(height: 50,),
+                    ],
+                  )),
+
+
+                  ReusableBottom(buttonName: 'LogIn',color: Colors.tealAccent,),
+                ],
+              ),
+            ),
+            SizedBox(height: 50,),
+            ReusableBottom(buttonName: 'Create Account',color: Colors.deepOrangeAccent,),
+            SizedBox(height: 50,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/fbicon.png'),
+                SizedBox(width: 20,),
+                Image.asset('assets/snapicon.png'),
+                SizedBox(width: 20,),
+                Image.asset('assets/gmailicon.png')
+              ],
+            ),
+            Spacer()
+
+          ],
         ),
       ),
     );
